d8d00f6e3e2b341e87cc5951dec5421ee0a540d9
Javier Casado García <112375352+javiercasadogarcia@users.noreply.github.com>
2024-11-17 12:06:25 +0100

    Update _main.sh

--- scripts/core/_main.sh
+++ scripts/core/_main.sh
@@ -1,5 +1,5 @@
 if ! ${DOT_MAIN_SOURCED:-false}; then
-	for file in $DOTLY_PATH/scripts/core/{args,collections,documentation,dot,dotfiles,dotly,git,log,platform,output,script,str,table}.sh; do
+	for file in $DOTLY_PATH/scripts/core/{args,collections,documentation,dot,git,log,output,platform,script,str,table}.sh; do
 		source "$file"
 	done
 	unset file
