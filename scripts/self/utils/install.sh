e22caf4ba818744f70beb3e0e41c99f9dfe8f593
Javier Casado García <112375352+javiercasadogarcia@users.noreply.github.com>
2024-11-17 12:27:12 +0100

    Update install.sh

--- scripts/self/utils/install.sh
+++ scripts/self/utils/install.sh
@@ -34,10 +34,29 @@ install_macos_custom() {
 }
 
 install_linux_custom() {
+	if ! platform::command_exists brew; then
+		output::error "brew not installed, installing"
+
+		if [ "$DOTLY_ENV" == "CI" ]; then
+			export CI=1
+		fi
+
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+
+ 	mkdir -p "$HOME/bin"
+
 	output::answer "Installing needed gnu packages"
- 	sudo apt install zsh
-  	sudo apt install make
-   	sudo apt install hyperfine
+ 	#sudo apt install zsh
+  	#sudo apt install make
+   	#sudo apt install hyperfine
+    	brew list zsh || brew install zsh | log::file "Installing brew zsh"
+	brew list coreutils || brew install coreutils | log::file "Installing brew coreutils"
+	brew list make || brew install make | log::file "Installing brew make"
+	brew list gnu-sed || brew install gnu-sed | log::file "Installing brew gnu-sed"
+	brew list findutils || brew install findutils | log::file "Installing brew findutils"
+	brew list bat || brew install bat | log::file "Installing brew bat"
+	brew list hyperfine || brew install hyperfine | log::file "Installing brew hyperfine"
 }
 
 backup_files() {
